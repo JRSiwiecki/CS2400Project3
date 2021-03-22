5b59b5ed71580a8941c57dbdaedb3a9e6c04d8c0
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-03-22 15:01:06 -0700

    Improved output of DriverBT.

--- DriverBT.java
+++ DriverBT.java
@@ -11,13 +11,15 @@ public class DriverBT
 		BinaryTree<String> aTree = new BinaryTree<>();
 		createTree1(aTree);
 		
-		System.out.println("(binaryTree) post-order: ");
+		System.out.print("(binaryTree) post-order: ");
 		aTree.postorderTraverse();
 		
-		System.out.println("(binaryNode) post-order: ");
+		System.out.println();
+		
+		System.out.print("(binaryNode) post-order: ");
 		aTree.postorderTraverse_callBinaryNodeMethod();
 		
-		System.out.println();
+		System.out.println("\n");
 		
 		System.out.println("(BinaryTree) Height of tree is " + aTree.getHeight());
 		System.out.println("(BinaryNode) Height of tree is " + aTree.getHeight_callBinaryNodeMethod());
@@ -27,20 +29,21 @@ public class DriverBT
 		System.out.println("(BinaryTree) # nodes of tree is " + aTree.getNumberOfNodes());
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree.getNumberOfNodes_callBinaryNodeMethod());
 
-		System.out.println("==========================================");
-		System.out.println();
+		System.out.println("\n==========================================");
 		
 		System.out.println("2nd Testing Example:");
 		BinaryTree<String> aTree2 = new BinaryTree<>();
 		createTree2(aTree2);
 		
-		System.out.println("(binaryTree) post-order: ");
+		System.out.print("(binaryTree) post-order: ");
 		aTree2.postorderTraverse();
 		
-		System.out.println("(binaryNode) post-order: ");
+		System.out.println();
+		
+		System.out.print("(binaryNode) post-order: ");
 		aTree2.postorderTraverse_callBinaryNodeMethod();
 		
-		System.out.println();
+		System.out.println("\n");
 		
 		System.out.println("(BinaryTree) Height of tree is " + aTree2.getHeight());
 		System.out.println("(BinaryNode) Height of tree is " + aTree2.getHeight_callBinaryNodeMethod());
@@ -50,7 +53,7 @@ public class DriverBT
 		System.out.println("(BinaryTree) # nodes of tree is " + aTree2.getNumberOfNodes());
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree2.getNumberOfNodes_callBinaryNodeMethod());
 		
-		System.out.println("Done.");
+		System.out.println("\nDone.");
 	}  
 
 	public static void createTree1(BinaryTree<String> tree)
