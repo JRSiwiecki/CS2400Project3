8dc4d3b06a57718e3595a19d5314e46cd0447836
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-03-22 15:10:43 -0700

    Initial implementation of JUnit test cases.

---
+++ EdgeCases.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test class for testing edge cases.
+ * @author Joseph
+ *
+ */
+class EdgeCases 
+{
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
