c5f55b4272b73dc5ed5c5a4b78f41aea04b34c43
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-03-22 15:41:17 -0700

    Added comments to UninitTreeTest.

--- EdgeCases.java
+++ EdgeCases.java
@@ -18,4 +18,18 @@ class EdgeCases
 		assertThrows(EmptyTreeException.class, () -> {tree.postorderTraverse(); });
 		assertThrows(EmptyTreeException.class, () -> {tree.getNumberOfNodes(); });
 	}	
+	
+	/* No real way to test if a variable/object is uninitialized, no? You can 
+	 * test if it is null I suppose but that is only works if the variable/object
+	 * was initialized beforehand?
+	@Test
+	void UninitTreeTest()
+	{
+		BinaryTree<String> tree;
+		
+		assertThrows(EmptyTreeException.class, () -> {tree.getHeight(); });
+		assertThrows(EmptyTreeException.class, () -> {tree.postorderTraverse(); });
+		assertThrows(EmptyTreeException.class, () -> {tree.getNumberOfNodes(); });
+	}
+	*/
 }
